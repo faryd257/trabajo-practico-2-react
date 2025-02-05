19b71af83205b6bc3020e98d3d1b3deab33f8294
faryd ignacio ortíz <faryd257@gmail.com>
2025-02-04 21:29:37 -0300

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,31 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
 
-import { Link } from "react-router-dom"
-import "./Header.css"
-const Header = ()=>{
-    return(
-       <header>
-<div className="logo">
-<img src="./public/2.jpg" alt="logo" />
-</div>
-       
-<nav><ul>
-    
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/Productos">Productos</Link></li>
-            <li><Link to="/Login">Login</Link></li>
-            <li><Link to="/Registro">Registro</Link></li> 
-            </ul>
-            </nav>
-       </header>
-      
-    )
-}
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+      <img src="/2.jpg" alt="logo" />
 
-export{Header}
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Productos">Productos</Link>
+          </li>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+          <li>
+            <Link to="/Registro">Registro</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export { Header };
