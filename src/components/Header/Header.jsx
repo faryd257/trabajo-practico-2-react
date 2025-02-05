6eb213ca984cbc8bea00c8b19c9dd5fff1086259
faryd ignacio ortíz <faryd257@gmail.com>
2025-02-04 22:17:31 -0300

    terminado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  const cerrarMenu = () => {
+    setMenuAbierto(false);
+  };
+
   return (
     <header>
       <div className="logo">
-      <img src="/2.jpg" alt="logo" />
-
+        <img src="/2.jpg" alt="logo" />
       </div>
-      <nav>
+      <button className="menu-icono" onClick={() => setMenuAbierto(!menuAbierto)}>
+        ☰
+      </button>
+      <nav className={menuAbierto ? "menu abierto" : "menu"}>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={cerrarMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/Productos">Productos</Link>
+            <Link to="/Productos" onClick={cerrarMenu}>
+              Productos
+            </Link>
           </li>
           <li>
-            <Link to="/Login">Login</Link>
+            <Link to="/Login" onClick={cerrarMenu}>
+              Login
+            </Link>
           </li>
           <li>
-            <Link to="/Registro">Registro</Link>
+            <Link to="/Registro" onClick={cerrarMenu}>
+              Registro
+            </Link>
           </li>
         </ul>
+        <button className="cerrar-menu" onClick={cerrarMenu}>
+          Cerrar
+        </button>
       </nav>
     </header>
   );
